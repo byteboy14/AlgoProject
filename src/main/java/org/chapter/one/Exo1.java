83b0483a040da93c30d9732a07ddcd9a62a7f3a0
Tedi KAKATSI <byteboy14@gmail.com>
2026-09-03 16:34:02 +0000

    feat(exo1) : implementation done

---
+++ src/main/java/org/chapter/one/Exo1.java
@@ -0,0 +1,18 @@
+package org.chapter.one;
+
+public class Exo1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    Boolean[][] table = {
+        { true, true, false },
+        { false, true, false },
+        { false, true, true },
+    };
+    for (Boolean[] rows : table) {
+      for (Boolean col : rows) {
+        System.out.println(" value " + BooleanConverter.getConvertedValueOf(col));
+      }
+    }
+  }
+}
