68f199b275ee21a238d24165f40222f48195cc96
Tedi KAKATSI <byteboy14@gmail.com>
2026-09-08 09:18:48 +0000

    feat(logarithm) : create lg base two classes

---
+++ src/main/java/org/chapter/one/Logarithm.java
@@ -0,0 +1,13 @@
+package org.chapter.one;
+
+public class Logarithm {
+
+  public static Integer lg(int n) {
+    if (n == 0)
+      return null;
+
+    if (n == 2)
+      return 1;
+    return n;
+  }
+}
---
+++ src/test/java/org/chapter/one/LogarithmTest.java
@@ -0,0 +1,21 @@
+package org.chapter.one;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Assertions;
+
+class LogarithmTest {
+
+  @Test
+  void lg_0_expect_null() {
+    Assertions.assertNull(Logarithm.lg(0));
+  }
+
+  @ParameterizedTest
+  @CsvSource({ "2, 1" })
+  void lg_2_expect_one(int input, int expected) {
+    Assertions.assertEquals(expected, Logarithm.lg(input));
+  }
+
+}
